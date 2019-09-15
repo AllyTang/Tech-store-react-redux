76cf0cbf0dd437c64e757e4167c112db0aa77002
allytang <allytang2015@gmail.com>
2019-09-15 10:12:42 +1000

    Updates

---
+++ precache-manifest.b340232dab4461884a299cba1a659db6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0b880a77f6c606a9e70",
+    "url": "/Tech-store-react-redux/static/js/runtime~main.a0b880a7.js"
+  },
+  {
+    "revision": "648ce32d991f880a4117",
+    "url": "/Tech-store-react-redux/static/js/main.648ce32d.chunk.js"
+  },
+  {
+    "revision": "09c37d1714a8c553bbe2",
+    "url": "/Tech-store-react-redux/static/js/2.09c37d17.chunk.js"
+  },
+  {
+    "revision": "648ce32d991f880a4117",
+    "url": "/Tech-store-react-redux/static/css/main.e25db28e.chunk.css"
+  },
+  {
+    "revision": "e6bd2f9e78f941c3b06fd7649e217254",
+    "url": "/Tech-store-react-redux/index.html"
+  }
+];
